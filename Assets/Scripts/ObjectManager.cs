6da2aa30fa33bd23826943d0c3caad846d2a9639
the426518 <zacharyczech@gmail.com>
2025-07-05 15:40:21 +0200

    EVEYRTHING

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -1,39 +1,50 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Collider2D))]
 public class ObjectManager : MonoBehaviour
 {
-    public bool useText = true;
-    public TextAnimation textTarget;
+    public bool useText = false;
+    TextAnimation textTarget;
     public string newText;
 
-    public bool useHumanity = true;
-    public Humanity humanity;
+    public bool useHumanity = false;
+    Humanity humanity;
     public int humanityAmount;
 
-    public bool useNote = true;
-    public NoteManager noteTarget;
+    public bool useNote = false;
+    NoteManager noteTarget;
     public int noteIndex;
 
     Camera cam;
     Collider2D self;
+    SpriteRenderer[] childSprites;
 
     void Awake()
     {
         cam = Camera.main;
         self = GetComponent<Collider2D>();
+
         if (textTarget == null) textTarget = FindObjectOfType<TextAnimation>(true);
         if (humanity == null) humanity = FindObjectOfType<Humanity>(true);
         if (noteTarget == null) noteTarget = FindObjectOfType<NoteManager>(true);
+
+        childSprites = GetComponentsInChildren<SpriteRenderer>(true);
+        foreach (var sr in childSprites)
+            if (sr.transform != transform) sr.enabled = false;
     }
 
     void Update()
     {
-        if (NoteManager.IsOpen) return;
-        if (!Mouse.current.leftButton.wasPressedThisFrame) return;
+        var pos = Mouse.current.position.ReadValue();
+        var hit = Physics2D.GetRayIntersection(cam.ScreenPointToRay(pos));
+        bool over = hit.collider == self;
 
-        var hit = Physics2D.GetRayIntersection(cam.ScreenPointToRay(Mouse.current.position.ReadValue()));
-        if (hit.collider != self) return;
+        foreach (var sr in childSprites)
+            if (sr.transform != transform) sr.enabled = over;
+
+        if (NoteManager.IsOpen || !Mouse.current.leftButton.wasPressedThisFrame || !over) return;
 
         if (useText && textTarget != null) textTarget.Play(newText);
         if (useHumanity && humanity != null) humanity.Add(humanityAmount);
